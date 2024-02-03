1d82f762025bad1c186070e68a3660cb168cd2c3
RCLobster <44736025+RCLobster@users.noreply.github.com>
2024-02-03 11:45:07 -0800

    set up model properties and relationships, seeded database

--- models/index.js
+++ models/index.js
@@ -5,12 +5,29 @@ const Tag = require('./Tag');
 const ProductTag = require('./ProductTag');
 
 // Products belongsTo Category
+Product.belongsTo(Category, {
+  foreignKey: 'category_id'
+});
 
 // Categories have many Products
+Category.hasMany(Product);
 
 // Products belongToMany Tags (through ProductTag)
+Product.belongsToMany(Tag, {
+  through: {
+    model: ProductTag,
+    unique: false
+  },
+  foreignKey: 'product_id',
+  as: 'product_tags',
+});
 
 // Tags belongToMany Products (through ProductTag)
+Tag.belongsToMany(Product, {
+  through: ProductTag,
+  foreignKey: 'tag_id',
+  as: 'tag_product'
+});
 
 module.exports = {
   Product,
